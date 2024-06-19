9f5ff2144f28fd9d40c63e98946bd73333817436
0x8D <mirocairo15@gmail.com>
2024-06-19 22:32:43 +0100

    add page details of each post with category and price and also host

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,16 +1,61 @@
+'use client';
+
 import { Reservation } from "@prisma/client";
-import { SafeListing } from "@/app/types";
+import { useMemo } from "react";
+
+import { SafeListing, SafeUser } from "@/app/types";
+import { categories } from "@/app/components/navbar/Categories";
+import Container from "@/app/components/Container";
+import ListingHead from "@/app/components/Listings/ListingHead";
+import ListingInfo from "@/app/components/Listings/ListingInfo";
 
 interface ListingClientProps {
     reservations?: Reservation[];
-    listing: SafeListing
+    listing: SafeListing & {
+        user: SafeUser
+    };
+    currentUser?: SafeUser | null;
 }
 
-const ListingClient = () => {
+const ListingClient: React.FC<ListingClientProps> = ({
+    listing,
+    currentUser
+}) => {
+    const category = useMemo(() => {
+        return categories.find((item) => item.label === listing.category);
+    }, [listing.category])
+
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <div className="max-w-screen-lg mx-auto">
+                <div className="flex flex-col gap-6">
+                    <ListingHead
+                        title={listing.title}
+                        imageSrc={listing.imageSrc}
+                        locationValue={listing.locationValue}
+                        id={listing.id}
+                        currentUser={currentUser}
+                    />
+                    <div className="
+                            grid
+                            grid-cols-1
+                            md:grid-cols-7
+                            md:gap-10
+                            mt-6
+                    ">
+                        <ListingInfo
+                            user={listing.user}
+                            category={listing.category}
+                            description={listing.description}
+                            roomCount={listing.roomCount}
+                            guestCount={listing.guestCount}
+                            bathroomCount={listing.bathroomCount}
+                            locationValue={listing.locationValue}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Container>
     );
 }
 
